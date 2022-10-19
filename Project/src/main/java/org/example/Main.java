693b364e00ed7a8a893d11356491b2a7e88ce9a0
Gahmadova <gahmadova89@.com>
2022-10-18 22:50:17 -0500

    initial java push

---
+++ Project/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create(/*config*/).get("/", ctx -> ctx.result("Hello World")).start(7070);
+
+}
+}
